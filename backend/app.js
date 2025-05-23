f69f8e7833751a0a9a24a027b41cd578a5398017
Patrício Samuel <patricioanjosjf@gmail.com>
2025-05-23 16:37:02 -0300

    configura cors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'
 import router from './src/routes.js';
 
 dotenv.config();
@@ -8,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({origin: process.env.FRONTEND_URL}))
 
 app.use(router)
 
